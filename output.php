d8999b1d541c0a76d20fd44a57a4d493bef41f79
Arun Persaud <arun@nubati.net>
2008-01-03 13:52:28 +0100

    BUGIFX: added Lance to copyright and changed to 2008
    small changes in the footer

--- output.php
+++ output.php
@@ -345,7 +345,7 @@ function output_footer()
 
   echo "</div>\n\n";
   echo "<div class=\"footer\">\n";
-  echo "  <p class=\"left\"> copyright 2006-2007 Arun Persaud <br />\n".
+  echo "  <p class=\"left\"> copyright 2006-2008 Arun Persaud, Lance Thornton <br />\n".
     "  Verwendung der [deutschen] Kartenbilder mit Genehmigung <br />der Spielkartenfabrik Altenburg GmbH,(c) ASS Altenburger <br />\n".
     "  - ASS Altenburger Spielkarten - Spielkartenfabrik Altenburg GmbH <br />\n".
     "  a Carta Mundi Company Email: info@spielkarten.com Internet: www.spielkarten.com</p>\n";
