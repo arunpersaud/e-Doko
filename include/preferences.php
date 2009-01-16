de0515836f4d26a2fae1ca04b5a4a7a399801e4a
Arun Persaud <arun@nubati.net>
2009-01-15 21:52:17 -0800

    BUGFIX: forgot to initialize two variables in preferences.php

--- include/preferences.php
+++ include/preferences.php
@@ -12,11 +12,13 @@ if(!$myid)
   return;
 
 /* track what got changed */
-$changed_notify	   = 0;
-$changed_password  = 0;
-$changed_cards	   = 0;
-$changed_timezone  = 0;
-$changed_autosetup = 0;
+$changed_notify	      = 0;
+$changed_password     = 0;
+$changed_cards	      = 0;
+$changed_timezone     = 0;
+$changed_autosetup    = 0;
+$changed_sorting      = 0;
+$changed_openforgames = 0;
 
 display_user_menu($myid);
 
