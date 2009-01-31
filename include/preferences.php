ee08bf05ddb0bb5722b0f8b1569d7ab4473a798d
Arun Persaud <arun@nubati.net>
2009-01-30 19:32:21 -0800

    CLEANUP: clean up the preferences html output a bit
    should be better aligned html now

--- include/preferences.php
+++ include/preferences.php
@@ -19,6 +19,7 @@ $changed_timezone     = 0;
 $changed_autosetup    = 0;
 $changed_sorting      = 0;
 $changed_openforgames = 0;
+$changed_vacation     = 0;
 
 display_user_menu($myid);
 
@@ -28,10 +29,13 @@ $timezone =  DB_get_user_timezone($myid);
 
 DB_update_user_timestamp($myid);
 
-/* does the user want to change some preferences? */
+/* does the user want to change some preferences?
+ * update the database and track changes with a variable, so that
+ * we can later highlight the changed value
+ */
 if(myisset("timezone"))
   {
-    $newtimezone=$_REQUEST['timezone'];
+    $newtimezone = $_REQUEST['timezone'];
     if($newtimezone != $timezone)
       {
 	DB_query("UPDATE User SET timezone=".DB_quote_smart($newtimezone).
@@ -161,98 +165,107 @@ if(myisset("password0") &&  $_REQUEST["password0"]!="" )
 $PREF     = DB_get_PREF($myid);
 $timezone = DB_get_user_timezone($myid);
 
-/* output settings */
+/*
+ * output settings
+ */
 
 echo "<div class=\"user\">\n";
 echo "  <form action=\"index.php?action=prefs\" method=\"post\">\n";
 echo "  <h2>Your settings are</h2>\n";
-echo "    <table>\n";
-echo "    <tr><td>Email:                 </td><td> $email    </td></tr>\n";
-echo "    <tr><td>Timezone:              </td><td>";
+echo "    <fieldset>\n";
+echo "    <legend>Game-related</legend>\n";
+
+echo "      <table>\n";
+echo "        <tr><td>Notification:          </td><td>\n";
+echo "          <select id=\"notify\" name=\"notify\" size=\"1\">\n";
+if($PREF['email']=="emailaddict")
+  {
+    echo "            <option value=\"emailaddict\" selected=\"selected\">less emails</option>\n";
+    echo "            <option value=\"emailnonaddict\">lots of emails</option>\n";
+  }
+ else
+   {
+     echo "            <option value=\"emailaddict\">less email</option>\n";
+     echo "            <option value=\"emailnonaddict\" selected=\"selected\">lots of email</option>\n";
+   }
+echo "          </select>";
+if($changed_notify) echo "changed";
+echo " </td></tr>\n";
+
+echo "        <tr><td>Autosetup:          </td><td>\n";
+echo "          <select id=\"autosetup\" name=\"autosetup\" size=\"1\">\n";
+if($PREF['autosetup']=="yes")
+  {
+    echo "           <option value=\"yes\" selected=\"selected\">accept every game</option>\n";
+    echo "           <option value=\"no\">ask for games</option>\n";
+  }
+ else
+   {
+     echo "           <option value=\"yes\">accept every game</option>\n";
+     echo "           <option value=\"no\" selected=\"selected\">ask for games</option>\n";
+   }
+echo "         </select>";
+if($changed_autosetup) echo "changed";
+echo " </td></tr>\n";
+echo "    <tr><td>Sorting:          </td><td>\n";
+
+echo "         <select id=\"sorting\" name=\"sorting\" size=\"1\">\n";
+if($PREF['sorting']=="high-low")
+  {
+    echo "           <option value=\"high-low\" selected=\"selected\">high to low</option>\n";
+    echo "           <option value=\"low-high\">low to high</option>\n";
+  }
+ else
+   {
+     echo "           <option value=\"high-low\">high to low</option>\n";
+     echo "           <option value=\"low-high\" selected=\"selected\">low to high</option>\n";
+   }
+echo "         </select>";
+if($changed_sorting) echo "changed";
+echo " </td></tr>\n";
+echo "        <tr><td>Open for new games:          </td><td>\n";
+echo "         <select id=\"open_for_games\" name=\"open_for_games\" size=\"1\">\n";
+if($PREF['open_for_games']=="no")
+  {
+    echo "           <option value=\"yes\">yes</option>\n";
+    echo "           <option value=\"no\" selected=\"selected\">no</option>\n";
+  }
+ else /* default */
+   {
+     echo "           <option value=\"yes\" selected=\"selected\">yes</option>\n";
+     echo "           <option value=\"no\">no</option>\n";
+   }
+echo "         </select>";
+if($changed_openforgames) echo "changed";
+echo " </td></tr>\n";
+
+echo "    <tr><td>Card set:              </td><td>\n";
+echo "         <select id=\"cards\" name=\"cards\" size=\"1\">\n";
+if($PREF['cardset']=="altenburg")
+  {
+    echo "           <option value=\"altenburg\" selected=\"selected\">German cards</option>\n";
+    echo "           <option value=\"english\">English cards</option>\n";
+  }
+ else
+   {
+     echo "           <option value=\"altenburg\">German cards</option>\n";
+     echo "           <option value=\"english\" selected=\"selected\">English cards</option>\n";
+   }
+echo "         </select>";
+if($changed_cards) echo "changed";
+echo " </td></tr>\n";
+echo "      </table>\n";
+echo "    </fieldset>\n";
+echo "    <fieldset>\n";
+echo "      <legend>Personal</legend>\n";
+echo "      <table>\n";
+echo "        <tr><td>Email:                 </td><td> $email    </td></tr>\n";
+echo "        <tr><td>Timezone:              </td><td>\n";
 output_select_timezone("timezone",$timezone);
 if($changed_timezone) echo "changed";
 echo "</td></tr>\n";
-echo "    <tr><td>Notification:          </td><td>";
 
-echo "  <select id=\"notify\" name=\"notify\" size=\"1\">\n";
-      if($PREF['email']=="emailaddict")
-	{
-	  echo "   <option value=\"emailaddict\" selected=\"selected\">less emails</option>\n";
-	  echo "   <option value=\"emailnonaddict\">lots of emails</option>\n";
-	}
-      else
-	{
-	  echo "   <option value=\"emailaddict\">less email</option>\n";
-	  echo "   <option value=\"emailnonaddict\" selected=\"selected\">lots of email</option>\n";
-	}
-  echo "  </select>\n";
-if($changed_notify) echo "changed";
-echo " </td></tr>\n";
-echo "    <tr><td>Autosetup:          </td><td>";
-
-echo "  <select id=\"autosetup\" name=\"autosetup\" size=\"1\">\n";
-      if($PREF['autosetup']=="yes")
-	{
-	  echo "   <option value=\"yes\" selected=\"selected\">accept every game</option>\n";
-	  echo "   <option value=\"no\">ask for games</option>\n";
-	}
-      else
-	{
-	  echo "   <option value=\"yes\">accept every game</option>\n";
-	  echo "   <option value=\"no\" selected=\"selected\">ask for games</option>\n";
-	}
-  echo "  </select>\n";
-if($changed_autosetup) echo "changed";
-echo " </td></tr>\n";
-echo "    <tr><td>Sorting:          </td><td>";
-
-echo "  <select id=\"sorting\" name=\"sorting\" size=\"1\">\n";
-      if($PREF['sorting']=="high-low")
-	{
-	  echo "   <option value=\"high-low\" selected=\"selected\">high to low</option>\n";
-	  echo "   <option value=\"low-high\">low to high</option>\n";
-	}
-      else
-	{
-	  echo "   <option value=\"high-low\">high to low</option>\n";
-	  echo "   <option value=\"low-high\" selected=\"selected\">low to high</option>\n";
-	}
-  echo "  </select>\n";
-if($changed_sorting) echo "changed";
-echo " </td></tr>\n";
-echo "    <tr><td>Open for new games:          </td><td>";
-
-echo "  <select id=\"open_for_games\" name=\"open_for_games\" size=\"1\">\n";
-      if($PREF['open_for_games']=="no")
-	{
-	  echo "   <option value=\"yes\">yes</option>\n";
-	  echo "   <option value=\"no\" selected=\"selected\">no</option>\n";
-	}
-      else /* default */
-	{
-	  echo "   <option value=\"yes\" selected=\"selected\">yes</option>\n";
-	  echo "   <option value=\"no\">no</option>\n";
-	}
-  echo "  </select>\n";
-if($changed_openforgames) echo "changed";
-echo " </td></tr>\n";
-echo "    <tr><td>Card set:              </td><td>";
-
-echo "  <select id=\"cards\" name=\"cards\" size=\"1\">\n";
-      if($PREF['cardset']=="altenburg")
-	{
-	  echo "   <option value=\"altenburg\" selected=\"selected\">German cards</option>\n";
-	  echo "   <option value=\"english\">English cards</option>\n";
-	}
-      else
-	{
-	  echo "   <option value=\"altenburg\">German cards</option>\n";
-	  echo "   <option value=\"english\" selected=\"selected\">English cards</option>\n";
-	}
-  echo "  </select>\n";
-if($changed_cards) echo "changed";
-echo " </td></tr>\n";
-echo "    <tr><td>Password(old):         </td><td>",
+echo "        <tr><td>Password(old):         </td><td>",
   "<input type=\"password\" id=\"password0\" name=\"password0\" size=\"20\" maxlength=\"30\" />";
 switch($changed_password)
   {
@@ -269,16 +282,16 @@ switch($changed_password)
     echo "changed";
     break;
   }
-echo  " </td></tr>\n";
-echo "    <tr><td>Password(new):         </td><td>",
+echo " </td></tr>\n";
+echo "        <tr><td>Password(new):         </td><td>",
   "<input type=\"password\" id=\"password1\" name=\"password1\" size=\"20\" maxlength=\"30\" />",
   " </td></tr>\n";
-echo "    <tr><td>Password(new, retype): </td><td>",
+echo "        <tr><td>Password(new, retype): </td><td>",
   "<input type=\"password\" id=\"password2\" name=\"password2\" size=\"20\" maxlength=\"30\" />",
   " </td></tr>\n";
-echo "    <tr><td><input type=\"submit\"  name=\"passwd\" value=\"set\" /></td>",
-  "<td></td></tr>\n";
-echo "    </table>\n";
+echo "      </table>\n";
+echo "    </fieldset>\n";
+echo "    <fieldset><legend>Submit</legend><input type=\"submit\"  name=\"passwd\" value=\"set\" /></fieldset>\n";
 echo "  </form>\n";
 echo "</div>\n";
 
